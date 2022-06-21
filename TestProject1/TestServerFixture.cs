acd7039b36b7b342f521285d6a7af4c0b2478a7d
Hazel Lessiter <39559927+HazelLessiter@users.noreply.github.com>
2022-06-21 19:36:46 +0100

    Add files via upload

---
+++ TestProject1/TestServerFixture.cs
@@ -0,0 +1,37 @@
+﻿using BeerUI.Configuration;
+using BeerUIApp;
+using BeerUIApp.UseCases;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Net.Http;
+
+namespace BeerUI.Tests
+{
+    public class TestServerFixture
+    {
+        public IHost Host { get; private set; }
+
+        public TestServerFixture()
+        {
+            Host = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.development.json", optional: false);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<HttpClient>();
+                    services.AddTransient<IBeerUseCase, BeerUseCase>();
+
+                    services.AddOptions<PunkAPIConfiguration>()
+                    .Configure<IConfiguration>((options, configuration) =>
+                    {
+                        options.BaseURL = configuration["PunkAPI:BaseURL"];
+                    });
+
+                }).Build();
+        }
+    }
+}
