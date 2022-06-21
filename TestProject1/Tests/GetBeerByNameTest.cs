2884111170d2a5ac4fcedd41fa341ad4fa612efb
Hazel Lessiter <39559927+HazelLessiter@users.noreply.github.com>
2022-06-21 19:37:09 +0100

    Add files via upload

---
+++ TestProject1/Tests/GetBeerByNameTest.cs
@@ -0,0 +1,33 @@
+﻿using BeerUIApp.UseCases;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Xunit;
+
+namespace BeerUI.Tests
+{
+    public class GetBeerByNameTest
+    {
+        private TestServerFixture _testServerFixture;
+        private IBeerUseCase _beerUseCase;
+
+        public GetBeerByNameTest()
+        {
+            _testServerFixture = new TestServerFixture();
+            _beerUseCase = _testServerFixture.Host.Services.GetService<IBeerUseCase>();
+        }
+
+        [Fact]
+        public void GetBeerByName()
+        {
+            //Arrange
+            var name = "Buzz";
+
+            //Act
+            var beers = _beerUseCase.GetBeer(name);
+
+            //Assert
+            Assert.NotNull(beers);
+            Assert.Equal(name, beers.FirstOrDefault().Name);
+        }
+    }
+}
---
+++ TestProject1/Tests/GetBeersTest.cs
@@ -0,0 +1,33 @@
+﻿using BeerUIApp.UseCases;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Xunit;
+
+namespace BeerUI.Tests
+{
+    public class GetBeersTest
+    {
+        private TestServerFixture _testServerFixture;
+        private IBeerUseCase _beerUseCase;
+
+        public GetBeersTest()
+        {
+            _testServerFixture = new TestServerFixture();
+            _beerUseCase = _testServerFixture.Host.Services.GetService<IBeerUseCase>();
+        }
+
+        [Fact]
+        public void GetBeers()
+        {
+            //Arrange
+            var expectedCount = 15;
+
+            //Act
+            var beers = _beerUseCase.GetBeer(1);
+
+            //Assert
+            Assert.NotNull(beers);
+            Assert.Equal(expectedCount, beers.Count());
+        }
+    }
+}
